b0c7eb8ad8beba703bb44a9c82337c75a297821f
TrainBuilder <70177890+TrainBuilder@users.noreply.github.com>
2020-09-16 22:07:04 -0600

    Add files via upload

---
+++ email/src/com/company/Main.java
@@ -0,0 +1,101 @@
+package com.company;
+
+
+import java.util.Properties;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
+import javax.mail.Store;
+import java.io.IOException;
+import javax.mail.Multipart;
+import javax.mail.Part;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        String host = "pop.gmail.com";// change accordingly
+        String mailStoreType = "pop3";
+        String username = "teammoxieRus@gmail.com";// change accordingly
+        String password = "mox1e4u5";// change accordingly
+
+        check(host, mailStoreType, username, password);
+
+    }
+
+
+    public static void check(String host, String storeType, String user,
+                             String password) {
+        try {
+
+            //create properties field
+            Properties properties = new Properties();
+
+            properties.put("mail.pop3.host", host);
+            properties.put("mail.pop3.port", "995");
+            properties.put("mail.pop3.auth", "true");
+            properties.put("mail.pop3.starttls.enable", "true");
+            Session emailSession = Session.getDefaultInstance(properties);
+
+            //create the POP3 store object and connect with the pop server
+            Store store = emailSession.getStore("pop3s");
+
+            store.connect(host, user, password);
+
+            //create the folder object and open it
+            Folder emailFolder = store.getFolder("INBOX");
+            emailFolder.open(Folder.READ_ONLY);
+
+            // retrieve the messages from the folder in an array and print it
+            Message[] messages = emailFolder.getMessages();
+            System.out.println("messages.length---" + messages.length);
+
+            for (int i = 0, n = messages.length; i < n; i++) {
+                Message message = messages[i];
+                System.out.println("---------------------------------");
+                System.out.println("Email Number " + (i + 1));
+                System.out.println("Subject: " + message.getSubject());
+                System.out.println("From: " + message.getFrom()[0]);
+                System.out.println("Text: " + message.getContent());
+                String line = getMessageContent(message);
+                System.out.println(line);
+            }
+
+            emailFolder.close(false);
+            store.close();
+
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getMessageContent(Message message) throws MessagingException {
+        try {
+            Object content = message.getContent();
+            if (content instanceof Multipart) {
+                StringBuffer messageContent = new StringBuffer();
+                Multipart multipart = (Multipart) content;
+                for (int i = 0; i < multipart.getCount(); i++) {
+                    Part part = multipart.getBodyPart(i);
+                    if (part.isMimeType("text/plain")) {
+                        messageContent.append(part.getContent().toString());
+                    }
+                }
+                return messageContent.toString();
+            }
+            return content.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+    }
+
