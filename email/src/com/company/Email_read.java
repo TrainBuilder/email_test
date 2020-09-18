10255f52790ee5ad7776405cad86e3ca820fa78e
Cody <cmh.howard1@gmail.com>
2020-09-18 16:54:24 -0600

    returns object
    removed main program, check method now returns user object

---
+++ email/src/com/company/Email_read.java
@@ -0,0 +1,96 @@
+package com.company;
+
+import javax.mail.*;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Email_read {
+
+    static UserInfo check() {
+        String host = "pop.gmail.com";
+        String storeType = "pop3";
+        String user = "teammoxieRus@gmail.com";
+        String password = "mox1e4u5";
+
+        boolean empty = true;
+        UserInfo x1 = new UserInfo();;//user object
+        try {
+            while (empty) {
+                //create properties field
+                Properties properties = new Properties();
+
+                properties.put("mail.pop3.host", host);
+                properties.put("mail.pop3.port", "995");
+                properties.put("mail.pop3.starttls.enable", "true");
+                Session emailSession = Session.getDefaultInstance(properties);
+
+                //create the POP3 store object and connect with the pop server
+                Store store = emailSession.getStore("pop3s");
+
+                store.connect(host, user, password);
+
+                //create the folder object and open it
+                Folder emailFolder = store.getFolder("INBOX");
+                emailFolder.open(Folder.READ_ONLY);
+
+                // retrieve the messages from the folder in an array
+                Message[] messages = emailFolder.getMessages();
+                System.out.println("messages in INBOX: " + messages.length);
+
+
+                if (messages.length >= 1) empty = false;//new email in inbox; stop loop
+
+                for (int i = 0, n = messages.length; i < n; i++) {//for each message in the inbox
+                    Message message = messages[i];
+                    System.out.println("---------------------------------");
+                    System.out.println("Email Number " + (i + 1));
+                    System.out.println("From: " + message.getFrom()[0]);
+                    String line = getMessageContent(message);//read contents of email, return as string
+                    String[] userData = line.split(",");//separate sting by commas
+                    if (userData.length == 5) {//five subjects required
+                        x1.setDate(userData[0]);
+                        x1.setEmail(userData[1]);
+                        x1.setAddress(userData[2]);
+                        x1.setProduct(userData[3]);
+                        x1.setQuantity(userData[4]);//create new user with information from email
+                       // x1.print();
+                    }
+                }
+                emailFolder.close(false);
+                store.close();
+                Thread.sleep(5000);//check email every 5 seconds for new
+            }
+
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return x1;
+    }
+
+    //methode to convert email contents to string
+    public static String getMessageContent(Message message) throws MessagingException {
+        try {
+            Object content = message.getContent();
+            if (content instanceof Multipart) {
+                StringBuffer messageContent = new StringBuffer();
+                Multipart multipart = (Multipart) content;
+                for (int i = 0; i < multipart.getCount(); i++) {
+                    Part part = multipart.getBodyPart(i);
+                    if (part.isMimeType("text/plain")) {
+                        messageContent.append(part.getContent().toString());
+                    }
+                }
+                return messageContent.toString();
+            }
+            return content.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
--- email/src/com/company/UserInfo.java
+++ email/src/com/company/UserInfo.java
@@ -6,13 +6,24 @@ public class UserInfo {
         String Address;
         String product;
         String quantity;
-        public UserInfo(String dat, String mail,String Address, String product, String quantity){
+        public UserInfo(){}
+
+        void setDate(String dat){
             this.date = dat;
+        }
+        void setEmail(String mail){
             this.email = mail;
+        }
+        void setAddress(String Address){
             this.Address = Address;
+        }
+        void setProduct(String product){
             this.product = product;
+        }
+        void setQuantity(String quantity){
             this.quantity = quantity;
         }
+
         void print(){
             System.out.printf("Date: %s",date);
             System.out.printf("\nUser email: %s",email);
